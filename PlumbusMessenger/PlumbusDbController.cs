55eddc6b83908255d0cd548b4905c9151d372453
Maksim Oancha <maksimoancha12@gmail.com>
2018-06-04 00:54:26 +0300

    Add project files.

---
+++ PlumbusMessenger/PlumbusDbController.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlumbusMessenger
+{
+    public class PlumbusDbController
+    {
+        private string _connStr;
+
+        public PlumbusDbController(string connStr = @"Data Source=Y0LO\SQLEXPRESS;Initial Catalog=PlumbusDb;Integrated Security=True")
+        {
+            _connStr = connStr;
+        }
+
+        #region User controllers
+        public void AddUser(string name, string login, string pwd)
+        {
+            string sqlExpression = "spr_AddUser";
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@name",
+                    Value = name
+                };
+                SqlParameter loginParam = new SqlParameter
+                {
+                    ParameterName = "@login",
+                    Value = login
+                };
+                SqlParameter pwdParam = new SqlParameter
+                {
+                    ParameterName = "@pwd",
+                    Value = pwd
+                };
+                command.Parameters.Add(nameParam);
+                command.Parameters.Add(loginParam);
+                command.Parameters.Add(pwdParam);
+
+                var res = command.ExecuteScalar();
+            }
+        }
+
+        public List<UserObj> GetUsers()
+        {
+            string sqlExpression = "spr_GetAllUsers";
+            List<UserObj> list = new List<UserObj>();
+
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+                        string login = reader.GetString(2);
+                        string pwd = reader.GetString(3);
+                        list.Add(new UserObj(id, name, login, pwd));
+                    }
+                }
+                reader.Close();
+                
+            }
+
+            return list;
+        }
+        #endregion
+
+        #region Message controllers
+        public void UpdateMessage(int id, string dateOut)
+        {
+            string sqlExpression = "spr_UpdateMessage";
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter idParam = new SqlParameter
+                {
+                    ParameterName = "@id",
+                    Value = id
+                };
+                SqlParameter dateOutParam = new SqlParameter
+                {
+                    ParameterName = "@dateOut",
+                    Value = dateOut
+                };
+                
+                command.Parameters.Add(idParam);
+                command.Parameters.Add(dateOutParam);
+
+                var res = command.ExecuteScalar();
+            }
+        }
+
+        public List<MessageObj> GetMessages()
+        {
+            string sqlExpression = "spr_GetAllMessages";
+            List<MessageObj> list = new List<MessageObj>();
+
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string message = reader.GetString(1);
+                        string dateIn = reader.GetString(2);
+                        string dateOut = reader.GetString(3);
+                        int senderId = reader.GetInt32(4);
+                        int rcptId = reader.GetInt32(5);
+                        list.Add(new MessageObj(id, message, dateIn, dateOut, senderId, rcptId));
+                    }
+                }
+                reader.Close();
+
+            }
+
+            return list;
+        }
+
+        public void AddMessage(string messg, string dateIn, int senderId, int rcptId)
+        {
+            string sqlExpression = "spr_AddMessage";
+            using (SqlConnection connection = new SqlConnection(_connStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter msgParam = new SqlParameter
+                {
+                    ParameterName = "@message",
+                    Value = messg
+                };
+                SqlParameter dateInParam = new SqlParameter
+                {
+                    ParameterName = "@dateIn",
+                    Value = dateIn
+                };
+                SqlParameter dateOutParam = new SqlParameter
+                {
+                    ParameterName = "@dateOut",
+                    Value = "--:--:--"
+                };
+                SqlParameter senderIdParam = new SqlParameter
+                {
+                    ParameterName = "@senderId",
+                    Value = senderId
+                };
+                SqlParameter rcptIdParam = new SqlParameter
+                {
+                    ParameterName = "@rcptId",
+                    Value = rcptId
+                };
+                command.Parameters.Add(msgParam);
+                command.Parameters.Add(dateInParam);
+                command.Parameters.Add(dateOutParam);
+                command.Parameters.Add(senderIdParam);
+                command.Parameters.Add(rcptIdParam);
+
+                var res = command.ExecuteScalar();
+            }
+        }
+
+        #endregion
+    }
+
+    public class UserObj
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Pwd { get; set; }
+        public int Id { get; set; }
+
+        public UserObj(int id, string name, string login, string pwd)
+        {
+            Id = id;
+            Name = name;
+            Login = login;
+            Pwd = pwd;
+        }
+    }
+
+    public class MessageObj
+    {
+        public int  Id { get; set; }
+        public string Messg { get; set; }
+        public string DateIn { get; set; }
+        public string DateOut { get; set; }
+        public int SenderId { get; set; }
+        public int RcptId { get; set; }
+
+        public MessageObj(int id, string messg, string dateIn, string dateOut, int senderId, int rcptId)
+        {
+            Id = id;
+            Messg = messg;
+            DateIn = dateIn;
+            DateOut = dateOut;
+            SenderId = senderId;
+            RcptId = rcptId;
+        }
+    }
+}
---
+++ PlumbusMessenger/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlumbusMessenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("192.168.1.249"), 1024);
+            //TcpListener listener = new TcpListener(IPAddress.Parse("159.224.233.249"), 1024);
+            listener.Start(10);
+            listener.BeginAcceptTcpClient(MyCallback, listener);
+            Console.WriteLine("Server started");
+            
+
+
+            Console.ReadKey();
+        }
+
+        private static void MyCallback(IAsyncResult ar)
+        {
+            string clientAdrr = "";
+            try
+            {
+                TcpListener listener = ar.AsyncState as TcpListener;
+                TcpClient client = listener.EndAcceptTcpClient(ar);
+                listener.BeginAcceptTcpClient(MyCallback, listener);
+
+                StreamReader reader = new StreamReader(client.GetStream(), Encoding.GetEncoding(866));
+                StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.GetEncoding(866));
+                IPAddress remoteAddr = (client.Client.RemoteEndPoint as IPEndPoint).Address;
+                clientAdrr = remoteAddr.ToString();
+
+                Console.WriteLine($"User connect from IP: {remoteAddr}");
+
+                writer.WriteLine($"Welcome to Plumbus! =^_^=");
+                bool isLogin = false;
+                string currName = null;
+                var controller = new PlumbusDbController();
+                while (true)
+                {
+                    if(isLogin)
+                        writer.Write($"<{currName}> Enter command => ");
+                    else
+                        writer.Write("Enter command => ");
+                    writer.Flush();
+                    string command = reader.ReadLine().Trim().ToUpper();
+                    switch (command)
+                    {
+                        case "HELP":
+                            writer.WriteLine("CHECK IN - регистрация нового пользователя(Имя, Логин, Пароль)");
+                            writer.WriteLine("LOGIN - вход под своим логином и паролем");
+                            writer.WriteLine("LOG OUT - выход с текущего пользователя");
+                            writer.WriteLine("LIST USERS - получить список Имен зарегистрированых пользователей");
+                            writer.WriteLine("SEND MESSAGE - отправить сообщение(указать само сообщение и Имя получателя)");
+                            writer.WriteLine("MESSAGES - перейти в меню просмотра сообщений");
+                            writer.WriteLine("QUIT - выход");
+                            break;
+
+                        case "CHECK IN":
+                            if(isLogin) break;
+                            writer.Write("Enter your name =>");
+                            writer.Flush();
+                            string regName = reader.ReadLine();
+                            writer.Write("Enter login =>");
+                            writer.Flush();
+                            string regLogin = reader.ReadLine();
+                            writer.Write("Enter password =>");
+                            writer.Flush();
+                            string regPwd = reader.ReadLine();
+                            
+                            var list = controller.GetUsers();
+                            if (list.Count(x => x.Name == regName) > 0)
+                            {
+                                writer.WriteLine("This name is already taken!");
+                            }
+                            else if (list.Count(x => x.Login == regLogin) > 0)
+                            {
+                                writer.WriteLine("This login is already taken!");
+                            }
+                            else
+                            {
+                                controller.AddUser(regName, regLogin, regPwd);
+                                writer.WriteLine("User added! Now you can Login");
+                            }
+                            writer.Flush();
+                            break;
+
+                        case "LIST USERS":
+
+                            writer.WriteLine("Users:\n");
+                            foreach (var user in controller.GetUsers())
+                            {
+                                writer.WriteLine(user.Name);
+                            }
+
+                            writer.WriteLine();
+                            writer.Flush();
+                            break;
+
+                        case "SEND MESSAGE":
+                            if(!isLogin)break;
+                            writer.Write("Enter name Recipient =>");
+                            writer.Flush();
+                            string nameRcpt = reader.ReadLine();
+                            if (!controller.GetUsers().Exists(x => x.Name == nameRcpt))
+                            {
+                                writer.Write("Incorrect name Recipient");
+                                break;
+                            }
+                            writer.Write("Enter message =>");
+                            writer.Flush();
+                            string messg = reader.ReadLine();
+                            writer.Flush();
+
+                            var dateIn = DateTime.Now.ToString("d");
+                            controller.AddMessage(messg,dateIn,
+                                controller.GetUsers().First(x => x.Name==currName).Id,
+                                controller.GetUsers().First(x=>x.Name==nameRcpt).Id);
+                            writer.WriteLine("Message was sent!");
+                            break;
+
+                        case "MESSAGES":
+                            if(!isLogin) break;
+                            var messages = controller.GetMessages();
+                            var users = controller.GetUsers();
+                            writer.WriteLine();
+                            while (true)
+                            {
+                                writer.WriteLine("Commands: UNREAD - Show new messages; ALL - show all messages; BACK - to main menu");
+                                writer.Write("Enter command => ");
+                                writer.Flush();
+                                command = reader.ReadLine().Trim().ToUpper();
+                                switch (command)
+                                {
+                                    case "UNREAD":
+                                        foreach (var mess in messages)
+                                        {
+                                            if (mess.RcptId == users.Find(x => x.Name == currName).Id &&
+                                                mess.DateOut == "--:--:--")
+                                            {
+                                                writer.WriteLine(
+                                                    $"From <{users.Find(x => x.Id == mess.SenderId).Name}> at {mess.DateIn}: {mess.Messg}");
+                                                controller.UpdateMessage(mess.Id, DateTime.Now.ToString("d"));
+                                            }
+                                        }
+                                        writer.WriteLine();
+                                        writer.Flush();
+                                        break;
+                                    case "ALL":
+                                        foreach (var mess in messages)
+                                        {
+                                            if (mess.RcptId == users.Find(x => x.Name == currName).Id )
+                                            {
+                                                writer.WriteLine(
+                                                    $"From <{users.Find(x => x.Id == mess.SenderId).Name}> at {mess.DateIn}: {mess.Messg}");
+                                                writer.WriteLine();
+                                            }
+                                        }
+                                        writer.WriteLine();
+                                        writer.Flush();
+                                        break;
+                                    case "BACK":
+                                        goto MESS;
+                                }
+                            }
+
+                            MESS:;
+                            break;
+
+                        case "LOGIN":
+                            writer.Write("Enter login =>");
+                            writer.Flush();
+                            string login = reader.ReadLine();
+                            writer.Write("Enter password =>");
+                            writer.Flush();
+                            string passwd = reader.ReadLine();
+                            if (isLogin = CheckUser(login, passwd, out string name))
+                            {
+                                writer.WriteLine();
+                                writer.WriteLine($"Login successed! Hello {name}");
+                                currName = name;
+                            }
+                            else
+                            {
+                                writer.WriteLine("Login failed");
+                            }
+                            writer.Flush();
+                            break;
+
+                        case "LOG OUT":
+                            if (isLogin)
+                            {
+                                writer.WriteLine($"\nYou are log out");
+                                isLogin = false;
+                                currName = null;
+                            }
+                            else
+                            {
+                                writer.WriteLine("Login failed");
+                            }
+                            writer.Flush();
+                            break;
+
+                        case "QUIT":
+                            writer.WriteLine("Good buy!!!");
+                            writer.Flush();
+                            goto END;
+
+                    }
+                }
+
+                END:;
+                Console.WriteLine($"Disonnect from IP: {remoteAddr}");
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Disonnect from IP: {clientAdrr}");
+            }
+        }
+
+        private static bool CheckUser(string login, string passwd, out string name)
+        {
+            var controller = new PlumbusDbController();
+            var list = controller.GetUsers();
+            var users = list.Where( x => x.Login == login && x.Pwd==passwd );
+            if (users.Count()!=0)
+            {
+                name = users.First().Name;
+                return true;
+            }
+
+            name = null;
+            return false;
+        }
+    }
+}
